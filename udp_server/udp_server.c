eb1614fb905b81235c22154fabfbf7802aba805b
n-maido <nhi_maido@outlook.com>
2023-04-04 20:26:48 -0700

    extract ip and pass to lcd

--- udp_server/udp_server.c
+++ udp_server/udp_server.c
@@ -133,13 +133,12 @@ static void processReply(char * msg, const unsigned int msgLen, char * r){
 
         char* name = strtok(contact, "&");
         char* sipAddress = strtok(NULL, "&");
+        char* tmp = strtok(sipAddress, "@");
+        char* ip = strtok(NULL, "@");
 
-        printf("new contact: %s, %s\n", name, sipAddress);
-        IFace_addUser(name, sipAddress);
+        printf("new contact: %s, %s\n", name, ip);
+        IFace_addUser(name, ip);
 
-        //TODO: call LCD_add_contact(name, sipAddress)
-
-        //TODO: return their sip address?
         strncpy(r, "{\"msgType\":\"add_contact\", \"content\": \"Success\"}\n", 100);
 
     } else if(!strncmp(msg, optionValues[DELETE_CONTACT], strlen(optionValues[DELETE_CONTACT]))){
